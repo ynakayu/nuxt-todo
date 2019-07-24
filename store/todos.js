7aa74ea575b566f5405dd7531aa3f3e4d2f55849
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-07-24 11:46:59 +0900

    add todos.js

---
+++ store/todos.js
@@ -0,0 +1,32 @@
+import firebase from '~/plugins/firebase'
+import { firestoreAction } from 'vuexfire'
+
+const db = firebase.firestore()
+const todosRef = db.collection('todos')
+
+export const state = () => ({
+  todos: []
+})
+
+export const actions = {
+  init: firestoreAction(({ bindFirestoreRef }) => {
+    bindFirestoreRef('todos', todosRef)
+  }),
+  add: firestoreAction((context, name) => {
+    if(name.trim()) {
+      todosRef.add({
+        name: name,
+        done: false,
+        created: firebase.firestore.FieldValue.serverTimestamp()
+      })
+    }
+  }),
+  remove: firestoreAction((context, id) => {
+    todosRef.doc(id).delete()
+  }),
+  toggle: firestoreAction((context, todo) => {
+    todosRef.doc(todo.id).update({
+      done: !todo.done
+    })
+  })
+}
