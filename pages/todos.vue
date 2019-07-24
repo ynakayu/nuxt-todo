ddd6666597f8feac770c35346a6e5f32215e4db8
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-07-24 14:18:15 +0900

    add todo

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-
+    <div class="form">
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    data: function() {
+      return {
+        name: '',
+        done: false
+      }
+    },
     created: function() {
       this.$store.dispatch('todos/init')
+    },
+    methods: {
+      add() {
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      }
     }
   }
 </script>
