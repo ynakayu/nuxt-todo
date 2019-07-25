9b56a5be84a2f8a3b3419d03f3909953f3937910
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-07-25 11:59:39 +0900

    bug fix 1

--- pages/todos.vue
+++ pages/todos.vue
@@ -4,14 +4,16 @@
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         <!-- {{ todo }} -->
-        <input 
-        type="checkbox"
-        v-bind:checked="todo.done"
-        @change="toggle(todo)">
-        <span v-bind:class="{ done: todo.done }">
-          {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
+        <span v-if="todo.created">
+          <input 
+          type="checkbox"
+          v-bind:checked="todo.done"
+          @change="toggle(todo)">
+          <span v-bind:class="{ done: todo.done }">
+            {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
+          </span>
+          <button v-on:click="remove(todo.id)">X</button>
         </span>
-        <button v-on:click="remove(todo.id)">X</button>
       </li>
     </ul>
     <div class="form">
@@ -61,7 +63,7 @@
 </script>
 
 <style>
-li > span.done {
+li > span > span.done {
   text-decoration: line-through;
 }
 </style>
